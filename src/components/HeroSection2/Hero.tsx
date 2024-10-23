5566aa528167acabd9ea4df580c1c28f6240c59c
Naman Omar <myselflearn7@gmail.com>
2024-10-23 19:55:38 +0530

    Update Hero.tsx

--- src/components/HeroSection2/Hero.tsx
+++ src/components/HeroSection2/Hero.tsx
@@ -8,6 +8,8 @@ const Header = () => {
         <br></br>
         <br></br>
         <br></br>
+        <br></br>
+        <br></br>
       <div className="flex flex-col md:flex-row flex-wrap  rounded-lg  px-6 md:px-10 lg:px-20 ">
         {/* left side */}
         <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py[10vw] md:mb-[30px]  ">
